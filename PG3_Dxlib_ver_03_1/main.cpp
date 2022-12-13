6f47c85284b2465e6b346ed123d42ec7d2d73f6f
LC1C_29 <k021g1384@g.neec.ac.jp>
2022-12-13 15:51:25 +0900

    03_01

--- PG3_Dxlib_ver_03_1/main.cpp
+++ PG3_Dxlib_ver_03_1/main.cpp
@@ -10,6 +10,7 @@ const int WIN_WIDTH = 600;
 // ウィンドウ縦幅
 const int WIN_HEIGHT = 400;
 
+
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,
 	_In_ int nCmdShow) {
 	// ウィンドウモードに設定
